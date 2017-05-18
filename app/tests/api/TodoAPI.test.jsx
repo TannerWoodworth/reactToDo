bcdb1de08cb6a18f195c70758d0a9ac97298908d
Tanner Woodworth <twoodworth2014@gmail.com>
2017-05-18 15:32:57 -0400

    added search and sort functionality w/ tests

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -41,4 +41,34 @@ describe('TodoAPI', () => {
 			expect(actualTodos).toEqual(todos);
 		});
 	});
+
+	describe('filterTodos', () => {
+		var todos = [{id: 1, completed: true, text: 'test task'}, {id: 2, completed: true, text: 'another test task'}, {id: 3, completed: false, text: 'final test task'}];
+
+		it('should return all items if showCompleted is true', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filteredTodos.length).toBe(3);
+		});
+
+		it('should return items if showCompleted is false', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+			expect(filteredTodos.length).toBe(1);
+		});
+
+		it('should sort by completed status', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filteredTodos[0].completed).toBe(false);
+		});
+
+		it('should filter todos by searchText', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, true, 'final');
+			expect(filteredTodos.length).toBe(1);
+		});
+
+		it('should return all todos if searchText is empty', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filteredTodos.length).toBe(3);
+		});
+
+	});
 });
