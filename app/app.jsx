d2715fb4ddfff2e95c766a375057856dc26279b3
Tanner Woodworth <twoodworth2014@gmail.com>
2017-05-25 15:54:42 -0400

    added redux and redux store

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,19 @@
 var React = require('react'),
-		ReactDOM = require('react-dom'),
-		{Route, Router, IndexRoute, hashHistory} = require('react-router');
+	ReactDOM = require('react-dom'),
+	{Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
+
+var actions = require('actions'),
+		store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 
 // Load foundation
 $(document).foundation();
